f43ff4aa87559e55ed799bc331fec83fdd735d32
sajid365-sr <sajidsorker86@gmail.com>
2022-11-08 04:10:49 +0600

    home and user route created also a complex form created

---
+++ src/components/AddUser.js
@@ -0,0 +1,39 @@
+
+    import React, { useState } from 'react';
+    
+    const AddUser = () => {
+
+        const [user, setUser] = useState({});
+        const handleAddUser = event =>{
+            
+            event.preventDefault();
+            console.log(user)
+
+        }
+
+    const handleInputBlur = event =>{
+        const field = event.target.name;
+        const value = event.target.value;
+        
+        const newUser = {...user}
+        newUser[field] = value;
+        setUser(newUser);
+        
+    }
+        return (
+            <div>
+                <h2>Please add a new user</h2>
+                <form onClick={handleAddUser}>
+                    <input onBlur={handleInputBlur} type="text" name='name' placeholder='name' required />
+                    <br />
+                    <input onBlur={handleInputBlur} type="text" name='address' placeholder='address' required />
+                    <br />
+                    <input onBlur={handleInputBlur} type="email" name='email' placeholder='email' required />
+                    <br />
+                    <button type='submit'>Add User</button>
+                </form>
+            </div>
+        );
+    };
+    
+    export default AddUser;
