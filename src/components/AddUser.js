6745e9a756d1ea34208f044ad1fbe976fb0ae471
sajid365-sr <sajidsorker86@gmail.com>
2022-11-08 10:38:07 +0600

    user data send to bakend server and then store in db complete

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -2,28 +2,52 @@
     import React, { useState } from 'react';
     
     const AddUser = () => {
+        
+        const [user, setUser] = useState({ });
 
-        const [user, setUser] = useState({});
+       
         const handleAddUser = event =>{
             
             event.preventDefault();
+            
             console.log(user)
+
+        // Sending data to the server 
+            fetch('http://localhost:5000/users', {
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(user)
+            })
+
+            .then(res => res.json())
+            .then(data =>{
+                if(data.acknowledged){
+                    alert('User added successfully');
+                    event.target.reset();
+                }
+            })
 
         }
 
-    const handleInputBlur = event =>{
-        const field = event.target.name;
-        const value = event.target.value;
-        
-        const newUser = {...user}
-        newUser[field] = value;
-        setUser(newUser);
-        
-    }
+        const handleInputBlur = event =>{
+            const field = event.target.name;
+            const value = event.target.value;
+            
+            const newUser = {...user}
+            newUser[field] = value;
+            setUser(newUser);
+            
+            
+        }
+
+
+   
         return (
             <div>
                 <h2>Please add a new user</h2>
-                <form onClick={handleAddUser}>
+                <form onSubmit={handleAddUser}>
                     <input onBlur={handleInputBlur} type="text" name='name' placeholder='name' required />
                     <br />
                     <input onBlur={handleInputBlur} type="text" name='address' placeholder='address' required />
