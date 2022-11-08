3e863a8c81d2c41e0aed5d30570690d3051b2c03
sajid365-sr <sajidsorker86@gmail.com>
2022-11-08 14:02:09 +0600

    delete operation done

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import AddUser from './components/AddUser';
 import Home from './components/Home';
+import Update from './components/Update';
 
 function App() {
   const router = createBrowserRouter([
     {
       path:'/',
-      element:<Home></Home>
+      element:<Home></Home>,
+      loader: () => fetch('http://localhost:5000/users')
     },
     {
       path:'/users/add',
       element:<AddUser></AddUser>
+    },
+    {
+      path:'/update/:id',
+      element:<Update></Update>
     }
   ])
 
---
+++ src/components/Update.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Update = () => {
+    return (
+        <div>
+            <h2>Please update: </h2>
+        </div>
+    );
+};
+
+export default Update;
